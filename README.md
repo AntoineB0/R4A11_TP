a0f05a520616a016bc99f459f5aa2e6795a26397
AntoineB0 <antoine.berteloot@etu.unilim.fr>
2025-01-24 11:58:05 +0100

    bientot fini

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # R4A11_TP1
-Berteloot AntoineTP développement d'application mobile
+Berteloot Antoine
+TP développement d'application mobile
+
+# TP1
+
+Question 1: app/src/main/java/com/example/tp1/MainActivity.kt
+
+Question 2: android:text="Coucou j'ai trouvé comment faire !"
+
+Question 3: android:icon="@mipmap/ic_launcher"
+
+            android:roundIcon="@mipmap/ic_launcher_round"
+
+
+
---
+++ TP1/app/src/main/java/com/example/tp1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tp1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val premierBouton : Button = findViewById(R.id.button)
+        val premiertextinput : TextInputLayout = findViewById(R.id.premiertextinput)
+        val premiertextview : TextView = findViewById(R.id.textview)
+        premierBouton.setOnClickListener {
+            premiertextview.text = premiertextinput.editText?.text.toString()
+        }
+
+    }
+}
